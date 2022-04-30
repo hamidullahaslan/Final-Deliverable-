7c76a8791747b1ace9ef5e8672643277776bed04
hamidullahaslan <92825080+hamidullahaslan@users.noreply.github.com>
2022-04-30 14:49:38 -0700

    Add files via upload
    This is the frontend code consist of html form.

---
+++ frontend.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<body>  
+
+<form action="FinalProject.php" method="POST">
+	User_ID (try >10): <input type="number" name="user_id">
+	<br>
+	Name: <input type="text" name="name">
+	<br>
+	User_Type (type "FREE_USER"): <input type="text" name="type">	
+	<br>
+	Ad-Watch-Time (in seconds): <input type="number" name="ad-watch-time"><br>
+	<input type="submit">
+</form>
+
+
+
+</body>
+</html>
